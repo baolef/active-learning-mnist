303b6f51f435a94a8affddb00e6371a0fc135156
Akash Kishore <akashkishore@Akashs-MacBook-Pro.local>
2023-04-17 15:59:38 -0400

    added minimization of expected risk sampling

--- main.py
+++ main.py
@@ -9,6 +9,66 @@ from sklearn.utils import shuffle
 from modAL.models import ActiveLearner
 from modAL.models.base import BaseEstimator
 import os
+import multiprocess as mp
+from copy import deepcopy
+import scipy as sp
+import timeit
+
+def minimize_expected_risk(classifier: ActiveLearner, X_pool: np.ndarray, n: int = 1) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Greedily samples n samples that minimize the expected risk from X_pool and return their indexes and values.
+    :param classifier: The classifier for which the labels are to be queried.
+    :param X_pool: The sample pool.
+    :param n: The number of queries.
+    :return: n random samples indexes and n random samples.
+    """
+    proc_pool = mp.Pool()
+
+    print("Initialised pool")
+
+    base_model = classifier.estimator
+    n_classes = len(np.unique(classifier.y_training))
+    X_u_prob = base_model.predict_proba(X_pool)
+    X_pool_cp = np.copy(X_pool)
+    tmp_model = deepcopy(classifier.estimator)
+
+
+    print(len(X_pool))
+
+    start = timeit.default_timer()
+    print(start)
+
+    def inner_pool_helper(i:int) -> float:
+        start = timeit.default_timer()
+        loss = 0
+        for label in range(n_classes):
+            tmp_x = np.append(classifier.X_training, [X_pool_cp[i,:]], axis = 0)
+            tmp_y = np.append(classifier.y_training, [str(label)], axis = 0)
+            tmp_model.fit(tmp_x, tmp_y)
+
+            probs = tmp_model.predict_proba(X_pool_cp)
+            prob_entropy = sp.stats.entropy(probs.T)
+            loss += np.sum(prob_entropy) * X_u_prob[i,label]
+        
+        stop = timeit.default_timer()
+        print('Time taken in seconds =',stop-start)
+        
+        return loss
+    
+    expected_risk = proc_pool.map(inner_pool_helper,range(len(X_pool)))
+    proc_pool.close()
+    proc_pool.join()
+
+    
+
+    if n == 1:
+        idx = np.argmin(expected_risk)
+        return idx, X_pool[idx]
+    else:
+        idx = np.argsort(expected_risk)[:n]
+        return idx. X_pool[idx]
+
+
 
 
 def uncertainty_sampling(classifier: ActiveLearner, X_pool: np.ndarray, n: int = 1) -> tuple[np.ndarray, np.ndarray]:
@@ -104,8 +164,8 @@ def pipeline(dataset: Dataset, model: BaseEstimator, query: Callable, label: str
     acc = []
     train_x, test_x, train_y, test_y = dataset.get()
     for _ in range(n):
-        train_x, train_y = shuffle(train_x, train_y)
-        accuracy = learning(train_x, train_y, test_x, test_y, model, query, base, samples, batch)
+        train_x, train_y = shuffle(train_x[:,:2], train_y)
+        accuracy = learning(train_x, train_y, test_x[:,:2], test_y, model, query, base, samples, batch)
         acc.append(accuracy)
     acc = np.array(acc)
     plot(acc, label, base, batch)
@@ -115,8 +175,9 @@ if __name__ == '__main__':
     dataset = Dataset()
     dataset.plot('data.png')
 
-    pipeline(dataset, SVC(probability=True), random_sampling, 'passive', 10, 90, 1, 2)
-    pipeline(dataset, SVC(probability=True), uncertainty_sampling, 'uncertainty', 10, 90, 1, 2)
+    pipeline(dataset, SVC(probability=True), minimize_expected_risk, 'min_exp_risk', 10, 90, 1, 2)
+    # pipeline(dataset, SVC(probability=True), random_sampling, 'passive', 10, 90, 1, 2)
+    # pipeline(dataset, SVC(probability=True), uncertainty_sampling, 'uncertainty', 10, 90, 1, 2)
 
     plt.savefig('result.png')
     plt.close()
