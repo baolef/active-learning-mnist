230182b088861c5f7ec6840308c842d86b0566f0
yuanch-en <64295131+yuanch-en@users.noreply.github.com>
2023-04-17 18:13:00 -0400

    Diversity Sampling

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from data import Dataset
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.svm import SVC
+from sklearn.cluster import KMeans
 from sklearn.utils import shuffle
 from modAL.models import ActiveLearner
 from modAL.models.base import BaseEstimator
@@ -17,30 +18,12 @@ def uncertainty_sampling(classifier: ActiveLearner, X_pool: np.ndarray, n: int =
     :param classifier: The classifier for which the labels are to be queried.
     :param X_pool: The sample pool.
     :param n: The number of queries.
-    :return: n indices from samples maximizing uncertainty and the n samples.
+    :return: n random samples indexes and n random samples.
     """
     uncertainty = 1 - np.max(classifier.predict_proba(X_pool), axis=1)
     idx = np.argsort(uncertainty)[-n:]
     return idx, X_pool[idx]
 
-def density_sampling(classifier: ActiveLearner, X_pool: np.ndarray, n: int = 1, beta: float = 1.0) -> tuple[np.ndarray, np.ndarray]:
-    """
-    Samples n samples with the largest product of uncertainty-based utility and average similarity to other samples in X_pool, returning their indices and values.
-    :param classifier: The classifier for which the labels are to be queried.
-    :param X_pool: The sample pool.
-    :param n: The number of queries.
-    :param beta: Hyperparameter exponentiating the average similarity term of the metric.
-    :return: n indices from samples maximizing the density-based metric and the n samples.
-    """
-    uncertainty = 1 - np.max(classifier.predict_proba(X_pool), axis=1)
-    num_points = len(X_pool)
-    avg_similarity = np.zeros(num_points)
-    for i in range(num_points):
-        dists = np.linalg.norm(np.delete(X_pool, i, 0) - X_pool[i], axis=1) # list of Euclidean distances between i-th sample and all others in pool
-        avg_similarity[i] = np.mean(1.0 / (1.0 + dists)) # 1 / (1 + d) converts distance measure to similarity measure with maximum of 1
-    idx = np.argsort(uncertainty * (avg_similarity ** beta))[-n:]
-    return idx, X_pool[idx]
-    
 
 def random_sampling(classifier: ActiveLearner, X_pool: np.ndarray, n: int = 1) -> tuple[np.ndarray, np.ndarray]:
     """
@@ -52,6 +35,22 @@ def random_sampling(classifier: ActiveLearner, X_pool: np.ndarray, n: int = 1) -
     """
     query_idx = np.random.choice(range(len(X_pool)), n, False)
     return query_idx, X_pool[query_idx]
+
+def diversity_sampling(classifier: ActiveLearner, X_pool: np.ndarray, n: int = 1):
+    if X_pool.shape[0] > n:
+        cluster = KMeans(n_clusters=n, random_state=0, n_init="auto")
+        labels = cluster.fit_predict(X_pool)
+        idx_pool = np.empty(n,dtype=int)
+
+        for k in range(n):
+            pool = np.argwhere(labels==k).reshape(-1)
+            
+            idx_pool[k] = np.random.choice(pool, size=1, replace=False)
+        
+    else:
+        idx_pool = np.array(range(X_pool.shape[0]))
+
+    return idx_pool, X_pool[idx_pool]
 
 
 def learning(train_x: np.ndarray, train_y: np.ndarray, test_x: np.ndarray, test_y: np.ndarray, model: BaseEstimator,
@@ -134,6 +133,7 @@ if __name__ == '__main__':
     dataset = Dataset()
     dataset.plot('data.png')
 
+    pipeline(dataset, SVC(probability=True), diversity_sampling, 'diversity', 10, 90, 5, 2)
     pipeline(dataset, SVC(probability=True), random_sampling, 'passive', 10, 90, 1, 2)
     pipeline(dataset, SVC(probability=True), uncertainty_sampling, 'uncertainty', 10, 90, 1, 2)
 
